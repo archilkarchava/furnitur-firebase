2cedd32078a336d1e9d317f81a2a926c5da2aafb
Archil Karchava <archil1997@gmail.com>
2019-10-26 17:13:32 +0500

    changed api to match on the client side

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,4 @@
 // tslint:disable-next-line: no-implicit-dependencies
-import { DocumentData, DocumentReference } from '@google-cloud/firestore';
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import * as nodemailer from 'nodemailer';
@@ -31,15 +30,7 @@ interface IProduct {
         name: string,
         namePlural: string
     },
-    // category: DocumentSnapshot
-    description?: string,
-    image?: string,
-    name: string,
-    oldPrice?: number,
-    price: number
-}
-interface IFirebaseRawProduct {
-    category: DocumentReference | DocumentData | undefined
+    id: string,
     description?: string,
     image?: string,
     name: string,
@@ -48,23 +39,20 @@ interface IFirebaseRawProduct {
 }
 
 interface IOrder {
-    user: { firstName: string, lastName: string, phoneNumber: string }, items: Array<{ item: IProduct, quantity: number }>, sum: number
-}
-interface IFirebaseRawOrder {
-    user: { firstName: string, lastName: string, phoneNumber: string }, items: [{ item: DocumentReference, quantity: number }], sum: number
+    customer: { firstName: string, lastName: string, phoneNumber: string }, items: Array<{ item: IProduct, quantity: number }>, sum: number
 }
 
 const sendEmail = async (order: IOrder) => {
-    const user = order.user;
+    const customer = order.customer;
     const items = order.items;
     const mailOptions = {
         from: `${functions.config().gmail.email}`,
         to: recievingEmail,
         subject: 'Furnitur новый заказ',
         text: `
-Пользователь ${user.firstName} ${user.lastName} ${user.phoneNumber} хочет сделать заказ на сумму ${order.sum}
+Пользователь ${customer.firstName} ${customer.lastName} ${customer.phoneNumber} хочет сделать заказ на сумму ${order.sum} рублей.
 Товары: 
-${items.map(({ item, quantity }) => `   ${item.category.name} ${item.name} - ${item.price} в количестве ${quantity} шт.,`).join('\n')}`
+${items.map(({ item, quantity }) => `   ${item.category.name} ${item.name} - ${item.price} руб. в количестве ${quantity} шт.,`).join('\n')}`
     };
     try {
         await mailTransport.sendMail(mailOptions);
@@ -75,31 +63,12 @@ ${items.map(({ item, quantity }) => `   ${item.category.name} ${item.name} - ${i
     return null;
 }
 
-export const sendEmailWithNewOrder = functions.firestore.document('orders/{orderId}').onWrite(async (change, context) => {
-    const newOrder: IFirebaseRawOrder | undefined = change.after.exists ? change.after.data() as IFirebaseRawOrder : undefined;
+export const sendEmailWithNewOrder = functions.firestore.document('orders/{orderId}').onCreate((snap, context) => {
+    const newOrder = snap.exists ? snap.data() : undefined;
     if (!newOrder) {
         console.error("Error: couldn't retrieve data from database");
         return null;
     }
     console.log(JSON.stringify(newOrder, null, 4));
-    const items: Array<{ item: IProduct, quantity: number }> = []
-    for (const itemData of newOrder.items) {
-        await itemData.item.get().then(async snap => {
-            const curItem: { item: IFirebaseRawProduct, quantity: number } = { item: snap.data() as IFirebaseRawProduct, quantity: itemData.quantity };
-
-            // console.log('itemData.item: ', itemData.item)
-            await curItem.item.category!.get().then(snap2 => {
-                curItem.item.category = snap2.data();
-                // console.log('curItem.item.category: ', curItem.item.category)
-                items.push(curItem as { item: IProduct, quantity: number });
-                // console.log(curItem);
-            })
-        })
-    }
-    const orderData = {
-        ...newOrder,
-        items: items
-    }
-    console.log('orderData: ', JSON.stringify(orderData, null, 4));
-    return sendEmail(orderData)
+    return sendEmail(newOrder as IOrder)
 });
